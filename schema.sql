865660d9885330b30fe9947562f3a1d5386c7aae
zeke-barrera <zekebarrera@live.com>
2017-12-28 13:50:47 -0700

    Added get/list functions to app.py and db schema

---
+++ schema.sql
@@ -0,0 +1,14 @@
+drop table if exists apirelease;
+create table apirelease(
+        buildtime date,
+        version varchar(30) primary key,
+        links varchar2(30), methods varchar2(30)
+);
+
+drop table if exists users;
+create table users(
+        username varchar2(30),
+        emailid varchar2(30),
+        password varchar2(30), full_name varchar(30),
+        id integer primary key autoincrement
+); 
